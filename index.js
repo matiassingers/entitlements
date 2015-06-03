92e3bd9cff930295d05b84b4f31ab69fd2964fb3
Patrick <plondema@service2media.com>
2015-06-03 17:05:51 +0200

    Check if node is running on darwin before running a darwin only command

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 'use strict';
 
-var exec = require('child_process').exec;
 var plist = require('simple-plist');
+var os = require('os').platform();
 
 module.exports = function(path, callback){
-  exec('codesign -d --entitlements :- ' + path, function(error, output){
-    if(error){
-      return callback(error);
-    }
-
-    callback(null, plist.parse(output));
-  });
+  if(os === 'darwin') {
+    var exec = require('child_process').exec;
+    exec('codesign -d --entitlements :- ' + path, function(error, output){
+      if(error){
+        return callback(error);
+      }
+      console.log(output);
+      callback(null, plist.parse(output));
+    });
+  } else {
+    console.log('nothing');
+    callback(null, {});
+  }
 };
