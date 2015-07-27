9e7f70053d62afa798ee1c01491669949433f950
Patrick <plondema@service2media.com>
2015-07-27 14:23:12 +0200

    Added quotes around path part of command for cases where the path contains spaces

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 'use strict';
 
+var exec = require('child_process').exec;
 var plist = require('simple-plist');
-var os = require('os').platform();
 
 module.exports = function(path, callback){
-  if(os === 'darwin') {
-    var exec = require('child_process').exec;
-    exec('codesign -d --entitlements :- ' + path, function(error, output){
-      if(error){
-        return callback(error);
-      }
-      console.log(output);
-      callback(null, plist.parse(output));
-    });
-  } else {
-    console.log('nothing');
-    callback(null, {});
-  }
+  exec('codesign -d --entitlements :- "' + path + '"', function(error, output){
+    if(error){
+      return callback(error);
+    }
+
+    callback(null, plist.parse(output));
+  });
 };
