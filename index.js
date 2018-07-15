9a717b1a830f375c331accc6f6c253b7af18505d
Matias Singers <mail@matiassingers.com>
2018-07-15 16:05:36 +0800

    Add shell-escape to properly escape unsafe `exec` arguments

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 var exec = require('child_process').exec;
 var plist = require('simple-plist');
+var shellescape = require('shell-escape');
 
 module.exports = function(path, callback){
-  exec('codesign -d --entitlements :- "' + path + '"', function(error, output){
+  var args = ['codesign', '-d', '--entitlements :-', path];
+
+  exec(shellescape(args), function(error, output){
     if(error){
       return callback(error);
     }
